e98464aca0ecbec20de29d944a60a94f879aaae9
chaouche oussama <oussamachaouch68@gmail.com>
2023-03-18 18:18:13 +0100

    Add existing project files to Git

--- index.js
+++ index.js
@@ -46,3 +46,68 @@ else{saisir.value += valButoon;
 
 
 })
+
+saisir.addEventListener("keypress",(event)=>{
+
+var allowedCharacters = /[0-9.+*/-]/;
+    var key = event.key;
+    console.log("key "+key+" allowed",allowedCharacters.test(key))
+    if (!allowedCharacters.test(key)) {
+        console.log("allowed");
+      event.preventDefault();
+    }
+
+})
+
+
+
+//toggle
+
+var main = document.body;
+
+
+
+
+  var buttonsToggle = document.getElementsByClassName("button_toggle");
+var arr = [...buttonsToggle];
+
+arr.forEach((element, index) => {
+  element.addEventListener("click", (event) => {
+    let themeClass = event.target.getAttribute('theme')
+    document.body.classList.remove("theme1","theme2","theme3");
+    document.body.classList.add(themeClass)
+    element.style.opacity = "1";
+    // if (index == 0) {
+    //     setTheme("theme1");
+    // } else if (index == 1) {
+    //     setTheme("theme2");
+    // } else {
+      
+    // }
+    arr
+      .filter(function (item) {
+        return item != element;
+      })
+      .forEach((item) => {
+        item.style.opacity = "0";
+      });
+  });
+});
+
+
+
+
+
+/*
+function setTheme(theme) {
+    var themes = ["theme1", "theme2"];
+    for (var i=0; i < themes.length; i++) {
+
+      var styleSheet = document.getElementById(themes[i]);
+      if (themes[i] == theme) {
+        styleSheet.removeAttribute("disabled");
+      } else {
+        styleSheet.setAttribute("disabled", "disabled");
+      }      
+    }
+}*/
